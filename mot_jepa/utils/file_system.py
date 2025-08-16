4973662089fea3041eddef7c0439e6d58d682d1d
Momir Adžemović <32575918+Robotmurlock@users.noreply.github.com>
2025-08-16 11:35:50 +0200

    Fix regex filtering bug in listdir

--- mot_jepa/utils/file_system.py
+++ mot_jepa/utils/file_system.py
@@ -19,5 +19,5 @@ def listdir(path: str, regex_filter: Optional[str] = None) -> List[str]:
     """
     files = [p for p in os.listdir(path) if not p.startswith('.')]
     if regex_filter is not None:
-        files = [p for p in os.listdir(path) if re.match(regex_filter, p)]
+        files = [p for p in files if re.match(regex_filter, p)]
     return files
