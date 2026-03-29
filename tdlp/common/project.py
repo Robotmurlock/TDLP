4efc0f004cb74ed728c5187aa479b7ff259a590b
Robotmurlock <momir.adzemovic@gmail.com>
2026-03-29 23:16:26 +0200

    ref: Reorganize configs/ into configs/tdlp/ and configs/od/
    Separate Hydra YAML configs (tdlp/) from mmdet Python configs (od/)
    for clearer directory structure. Update CONFIGS_PATH and model_config
    references accordingly.

--- tdlp/common/project.py
+++ tdlp/common/project.py
@@ -5,6 +5,6 @@ import os
 ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 SRC_PATH = os.path.join(ROOT_PATH, 'rt_motip')
 ASSETS_PATH = os.path.join(ROOT_PATH, 'assets')
-CONFIGS_PATH = os.path.join(ROOT_PATH, 'configs')
+CONFIGS_PATH = os.path.join(ROOT_PATH, 'configs', 'tdlp')
 OUTPUTS_PATH = os.path.join(ROOT_PATH, 'outputs')
 PLAYGROUND_PATH = os.path.join(ROOT_PATH, 'playground')
